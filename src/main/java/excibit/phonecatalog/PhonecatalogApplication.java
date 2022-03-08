15695788a0ab9d46cc913bceec7284dee2cc2698
cristiana-marcu <cristiana.marcu.n@gmail.com>
2022-03-08 17:47:55 +0100

    Created project through spring.io

---
+++ src/main/java/excibit/phonecatalog/PhonecatalogApplication.java
@@ -0,0 +1,13 @@
+package excibit.phonecatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonecatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhonecatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/excibit/phonecatalog/PhonecatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package excibit.phonecatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhonecatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
