83efdce2cf35bff025f7b974a853ec72ea00f479
cristiana-marcu <cristiana.marcu.n@gmail.com>
2022-03-12 10:33:25 +0100

    Undo problematic commit when trying to get the frontend to the repo

--- src/main/java/excibit/phonecatalog/phone/controller/PhoneController.java
+++ src/main/java/excibit/phonecatalog/phone/controller/PhoneController.java
@@ -3,12 +3,14 @@ package excibit.phonecatalog.phone.controller;
 import excibit.phonecatalog.phone.persistence.Phone;
 import excibit.phonecatalog.phone.persistence.PhoneRepository;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "*")
 public class PhoneController {
 
   private final PhoneRepository phoneRepository;
